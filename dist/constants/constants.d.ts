73f295e0fb2a301197fd80776818c609bebfc335
ianshih <ianshih2003@gmail.com>
2023-04-02 23:32:48 -0300

    Fix build

--- dist/constants/constants.d.ts
+++ dist/constants/constants.d.ts
@@ -1,7 +1,3 @@
 export declare const DEFAULT_PROTO_PATH = "node_modules/@fiu-fit/common/src/protos";
-export declare const RoleEnumToName: {
-    0: string;
-    1: string;
-    2: string;
-};
+export declare const RoleEnumToName: Record<number, string>;
 export declare const RoleNameToEnum: Record<string, number>;
