80a1318ebba29444ff9757fc97fe22fb34aeef46
ianshih <ianshih2003@gmail.com>
2023-04-02 23:24:28 -0300

    Add RoleNumToName, RoleNameToEnum maps

--- dist/constants/constants.d.ts
+++ dist/constants/constants.d.ts
@@ -1,1 +1,7 @@
 export declare const DEFAULT_PROTO_PATH = "node_modules/@fiu-fit/common/src/protos";
+export declare const RoleEnumToName: {
+    0: string;
+    1: string;
+    2: string;
+};
+export declare const RoleNameToEnum: Record<string, number>;
--- dist/constants/constants.js
+++ dist/constants/constants.js
@@ -1,4 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_PROTO_PATH = void 0;
+exports.RoleNameToEnum = exports.RoleEnumToName = exports.DEFAULT_PROTO_PATH = void 0;
 exports.DEFAULT_PROTO_PATH = 'node_modules/@fiu-fit/common/src/protos';
+exports.RoleEnumToName = {
+    0: 'Admin',
+    1: 'Athlete',
+    2: 'Trainer',
+};
+exports.RoleNameToEnum = {
+    Admin: 0,
+    Athlete: 1,
+    Trainer: 2,
+};
