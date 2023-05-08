a61487412b9ecab65d11b6cfa17135df76818b79
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-05-08 14:16:53 -0300

    Added the rest of the interfaces

---
+++ dist/interfaces/exercise.d.ts
@@ -0,0 +1,7 @@
+import { Category } from './category';
+export interface Exercise {
+    _id: string;
+    name: string;
+    description: string;
+    category: Category;
+}
---
+++ dist/interfaces/workoutExercise.d.ts
@@ -0,0 +1,8 @@
+import { Unit } from './unit';
+export interface WorkoutExercise {
+    exerciseId: string;
+    sets: number;
+    reps: number;
+    weight?: number;
+    unit: Unit;
+}
