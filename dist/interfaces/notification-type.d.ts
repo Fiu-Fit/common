040a290708f3f4221680366f322e090411a93530
Rocío <rplatini@fi.uba.ar>
2023-06-07 16:53:11 -0300

    Create enum NotificationType

---
+++ dist/interfaces/notification-type.d.ts
@@ -0,0 +1,4 @@
+export declare enum NotificationType {
+    GoalCompleted = 0,
+    NewMessage = 1
+}
---
+++ dist/interfaces/notification-type.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NotificationType = void 0;
+var NotificationType;
+(function (NotificationType) {
+    NotificationType[NotificationType["GoalCompleted"] = 0] = "GoalCompleted";
+    NotificationType[NotificationType["NewMessage"] = 1] = "NewMessage";
+})(NotificationType = exports.NotificationType || (exports.NotificationType = {}));
